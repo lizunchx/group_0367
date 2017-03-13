57f40291e2b21aa66691e8c95a59ad345f72deb1
Sibtain Sadruddin <sibtain.sadruddin@mail.utoronto.ca>
2017-03-12 21:18:21 -0400

    Added getter method

--- project/WareHouse Project/src/warehouseObject/Facia.java
+++ project/WareHouse Project/src/warehouseObject/Facia.java
@@ -1,16 +1,18 @@
 package warehouseObject;
 
 public class Facia {
- 
+
 	private String colour;
 	private String sku;
 	private boolean front;
-	
-	public Facia (String colour, String sku, boolean front)
-	{
+
+	public Facia(String colour, String sku, boolean front) {
 		this.colour = colour;
 		this.sku = sku;
 		this.front = front;
 	}
-	public String getFacia() {return sku;}
+
+	public String getSKU() {
+		return sku;
+	}
 }
